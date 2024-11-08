4b52ba98b22a0ba1c7ac18e9155eae8e3cf33135
AcrobaticRaccoon47 <145118070+cc2803@users.noreply.github.com>
2024-11-08 18:25:02 +0530

    Update bitstuffing.py
    Updated code according to problem statement specifications - length of data to be transmitted is 16 bits and flag is 8 bits (01111110)

--- Code/Python/bitstuffing.py
+++ Code/Python/bitstuffing.py
@@ -6,13 +6,13 @@ The Algorithm involves checking for existence of flag as a substring in the data
 the string data to ensure that the substring isnt mistaken for a flag and thus avoid faulty transmission of data.
 '''
 
-flag = "0110"
+flag = "01111110"
 
 while True:
     data = input("Enter data to be transmitted (BINARY): ")
     print()
 
-    if len(data)!=8:
+    if len(data)!=16:
         print("Incorrect data length entered, Please enter valid input!\n")
 
     for i in data:
@@ -22,8 +22,8 @@ while True:
     break #to exit this loop once valid input is obtained
 
 if flag in data:
-    index = data.find(flag)+len(flag)-1
-    updated_data = data[:index]+"1"+data[index:]
+    index = data.find(flag)+len(flag)-2
+    updated_data = data[:index]+"0"+data[index:]
     print(f"Updated data is:\t{updated_data}")
     
 else:
